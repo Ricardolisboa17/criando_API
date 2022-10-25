57ceec7bc5fb592f2ab206b758a638b1e7a72fab
Ricardo Lisboa <ricardo.lisboa17@hotmail.com>
2022-10-25 19:40:00 -0300

    melhorando minha API

--- config/routes.js
+++ config/routes.js
@@ -2,42 +2,63 @@ const express = require('express')
 const routes = express.Router()
 const fs = require('fs');
 
-const db = []
 routes.get('/', (req,res) =>{
-    return res.json(db)
-    
+    fs.readFile('arquivo.txt', 'utf8', (err, conteudo) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        let array = []
+
+        if(conteudo != ""){
+           let splitConteudo = conteudo.split("\n")
+            for(let i = 0; i < splitConteudo.length; i++){
+                if(splitConteudo[i] != ""){    
+                    let json = JSON.parse(splitConteudo[i])
+                    array.push(json)
+                }
+            }
+            return res.send(array)
+        }else{
+            return res.json(array)
+        }
+    });
 })
 
-routes.post('/add', (req,res) =>{
+routes.post('/add',(req,res) =>{
    const {nome, sobrenome, idade , cpf} = req.body
-
-   const obj = {
-        nome,
-        sobrenome,
-        idade, 
-        cpf
-    }
 
     if(nome == null || sobrenome == null  || idade == null || cpf == null){
         return res.status(400).json({Response: "insira todas as informaçoes"})
     }
 
+   const obj = {
+        name: nome,
+        surname: sobrenome,
+        age: idade, 
+        document: cpf
+    }
+
+    var conteudo = fs.readFileSync('arquivo.txt', 'utf8')
+
+    const splitConteudo = conteudo.split("\n")
+  
+    for(let i = 0; i < splitConteudo.length; i++){
+        if(splitConteudo[i] != ""){
+            let json = JSON.parse(splitConteudo[i])
+        
+            if(json.document == cpf){
+                return res.status(400).json({Response: "CPF já cadastrado"})
+            }
+        }
+    }
+
     fs.appendFile('arquivo.txt', JSON.stringify(obj) + "\n", function (err) {
         if (err) throw err;
-        console.log('Salvo com sucesso');
-    });
-    
-    fs.readFile('arquivo.txt', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(data);
     });
 
-    db.push(obj)
-    return res.status(200).json(obj)
-    
+    return res.status(200).json({Response: "Usuario cadastrado com sucesso"})
 })
 
 module.exports = routes
